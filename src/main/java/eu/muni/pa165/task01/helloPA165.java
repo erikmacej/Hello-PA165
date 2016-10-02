a847ec943324cfdd219e2aae8817a7f6961c5198
Erik Macej <erikmacej15@gmail.com>
2016-10-02 23:51:58 +0200

    first commit

---
+++ src/main/java/eu/muni/pa165/task01/helloPA165.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.muni.pa165.task01;
+
+/**
+ *
+ * @author Erik Macej,433 744
+ */
+public class helloPA165 {
+        
+    public static void main(String[] args) {
+        System.out.println("Hello, PA165");
+    }
+    
+}
